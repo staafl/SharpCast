a33506e11e781903b1656b9a4a7be547cab4d8c2
Velko Nikolov <velko.nikolov@tick42.com>
2021-12-17 13:08:53 +0200

    minor
    [master]

--- SharpCastConsole/Program.cs
+++ SharpCastConsole/Program.cs
@@ -37,52 +37,54 @@
             bool persistent = args.Any(x => x == "/persistent");
             bool debug = args.Any(x => x == "/debug");
 
-            new Thread(() =>
+            if (true)
             {
-                HttpListener listener = new HttpListener();
-                listener.Prefixes.Add($"http://{ip}:{port}/");
-                listener.Start();
-                while (true)
+                new Thread(() =>
                 {
-                    try
+                    HttpListener listener = new HttpListener();
+                    listener.Prefixes.Add($"http://{ip}:{port}/");
+                    listener.Start();
+                    while (true)
                     {
-                        HttpListenerContext context = listener.GetContext();
-                        HttpListenerRequest request = context.Request;
-                        if (debug)
+                        try
                         {
-                            Console.WriteLine("Got request");
+                            HttpListenerContext context = listener.GetContext();
+                            HttpListenerRequest request = context.Request;
+                            if (debug)
+                            {
+                                Console.WriteLine("Got request");
+                            }
+                            HttpListenerResponse response = context.Response;
+                            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                            Graphics gr = Graphics.FromImage(bmp);
+                            gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                            using (var ms = new MemoryStream())
+                            {
+                                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                                response.ContentLength64 = ms.Length;
+                                response.ContentType = "image/jpeg";
+                                System.IO.Stream output = response.OutputStream;
+                                output.Write(ms.ToArray(), 0, (int)ms.Length);
+                                output.Close();
+                            }
+                            if (!persistent)
+                            {
+                                break;
+                            }
                         }
-                        HttpListenerResponse response = context.Response;
-                        Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-                        Graphics gr = Graphics.FromImage(bmp);
-                        gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
-                        using (var ms = new MemoryStream())
+                        catch (Exception ex)
                         {
-                            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-                            response.ContentLength64 = ms.Length;
-                            response.ContentType = "image/jpeg";
-                            System.IO.Stream output = response.OutputStream;
-                            output.Write(ms.ToArray(), 0, (int)ms.Length);
-                            output.Close();
-                        }
-                        if (!persistent)
-                        {
-                            break;
+                            Console.WriteLine("Listener exception");
+                            Console.WriteLine(ex + "");
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Listener exception");
-                        Console.WriteLine(ex + "");
-                    }
-                }
-                listener.Stop();
-                Environment.Exit(0);
-            }).Start();
+                    listener.Stop();
+                    Environment.Exit(0);
+                }).Start();
+            }
 
             Player client = new Player(host);
             client.Connect();
-
             if (debug)
             {
                 client.MediaStatusChanged += (sender, status) =>
@@ -105,7 +107,6 @@
                         },
                         autoPlay: true);
 
-                    // client.Play();
                     if (!persistent)
                     {
                         break;
@@ -116,6 +117,7 @@
                     Console.WriteLine("Chromecast exception");
                     Console.WriteLine(e + "");
                 }
+
                 Thread.Sleep(interval * 1000);
             }
         }
