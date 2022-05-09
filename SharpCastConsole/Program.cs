92ddb68d9726c1c2047bcce028f140d804e4d18f
Velko Nikolov <velko.nikolov@tick42.com>
2022-05-09 23:26:45 +0300

    mac search
    [master]

--- SharpCastConsole/Program.cs
+++ SharpCastConsole/Program.cs
@@ -9,6 +9,8 @@
     using System.Net.Sockets;
     using System.Net.NetworkInformation;
     using System.Linq;
+    using System.Collections.Generic;
+    using System.Diagnostics;
 
     class Program {
 
@@ -27,7 +29,13 @@
 
         static void Main(string[] args)
         {
-            string host = args.FirstOrDefault(x => !x.StartsWith("/")) ?? "192.168.0.105";
+            string host = args.FirstOrDefault(x => !x.StartsWith("/")) ?? "192.168.0.101";
+
+            string mac = GetArg(args, "mac");
+            if (!string.IsNullOrWhiteSpace(mac))
+            {
+                host = IPMacMapper.FindIPFromMacAddress(mac.Replace(":", "-").ToLower()) ?? host;
+            }
 
             if (args.Any(x => x == "/pause"))
             {
@@ -203,6 +211,71 @@
             }
 
             return bmpArray;
+        }
+    }
+
+    public class IPMacMapper
+    {
+        private static List<IPAndMac> list;
+
+        private static StreamReader ExecuteCommandLine(String file, String arguments = "")
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.FileName = file;
+            startInfo.Arguments = arguments;
+
+            Process process = Process.Start(startInfo);
+
+            return process.StandardOutput;
+        }
+
+        private static void InitializeGetIPsAndMac()
+        {
+            if (list != null)
+                return;
+
+            var arpStream = ExecuteCommandLine("arp", "-a");
+            List<string> result = new List<string>();
+            while (!arpStream.EndOfStream)
+            {
+                var line = arpStream.ReadLine().Trim();
+                result.Add(line);
+            }
+
+            list = result.Where(x => !string.IsNullOrEmpty(x) && (x.Contains("dynamic") || x.Contains("static")))
+                .Select(x =>
+                {
+                    string[] parts = x.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    return new IPAndMac { IP = parts[0].Trim(), MAC = parts[1].Trim() };
+                }).ToList();
+        }
+
+        public static string FindIPFromMacAddress(string macAddress)
+        {
+            InitializeGetIPsAndMac();
+            IPAndMac item = list.SingleOrDefault(x => x.MAC == macAddress);
+            if (item == null)
+                return null;
+            return item.IP;
+        }
+
+        public static string FindMacFromIPAddress(string ip)
+        {
+            InitializeGetIPsAndMac();
+            IPAndMac item = list.SingleOrDefault(x => x.IP == ip);
+            if (item == null)
+                return null;
+            return item.MAC;
+        }
+
+        private class IPAndMac
+        {
+            public string IP { get; set; }
+            public string MAC { get; set; }
         }
     }
 }
